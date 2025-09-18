e8172017dc5cf046597a7875c935c8b4d62e65ae
Inami <inami202rk@gmail.com>
2025-09-18 16:28:39 +0900

    test: add SRS unit tests

--- lib/services/srs_service.dart
+++ lib/services/srs_service.dart
@@ -1,5 +1,6 @@
-﻿import 'dart:convert';
+import 'dart:convert';
 
+import 'package:meta/meta.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'settings_service.dart';
@@ -104,8 +105,24 @@ class SrsSummary {
 
 enum SrsRating { again, hard, good, easy }
 
+typedef PickDueFn = Future<List<String>> Function(
+  Iterable<String> keys, {
+  required int limit,
+  String strategy,
+  Map<String, int>? wrongs,
+  bool prioritizeWrongToggle,
+  Map<String, SrsState>? statesCache,
+});
+
 class SrsService {
   static const _storageKey = 'srs.v2';
+
+  static PickDueFn? _pickDueOverride;
+
+  @visibleForTesting
+  static void setPickDueOverride(PickDueFn? override) {
+    _pickDueOverride = override;
+  }
 
   static Future<Map<String, SrsState>> loadAll() async {
     final prefs = await SharedPreferences.getInstance();
@@ -363,7 +380,8 @@ class SrsService {
 
     final deduped = candidate.toSet().toList();
 
-    return pickDue(
+    final pick = _pickDueOverride ?? pickDue;
+    return pick(
       deduped,
       limit: limit ?? cap,
       strategy: effectiveStrategy,
